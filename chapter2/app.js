8dcfb89da0ee8bffb9e08b03674bedee0ad657f3
Iqrasherazi <157386998+Iqrasherazi@users.noreply.github.com>
2024-05-23 02:27:26 +0500

    done

---
+++ chapter2/app.js
@@ -0,0 +1,58 @@
+
+// username
+let username
+
+let myName =" Syeda Iqra";
+// declare variable titeld message
+let msessage;
+
+
+// Assign " Hello world" to the variables
+message =" Hello world";
+ 
+
+// Display message in alert boxx
+alert (message)
+
+// Student bio data
+let studentName =" Syeda Iqra";
+let studentAge = 20;
+let studentGrade ="A+";
+
+
+// dspaly the bio data in alert box
+alert( " Student Name:" + studentName);
+alert( " Student Age:"+ studentAge
+);
+alert( " Student Grade:" + studentGrade
+);
+
+// Questions 5
+
+alert("PIZZA\nPIZZ\nPIZ\nPI\nP");
+
+// Declare a variable called email and assign your email address to it
+let email = "example@example.com";
+
+// Display a message in an alert box using string concatenation
+alert("My email address is: " + email);
+
+// Declare a variable called book and assign it the value "A smarter way to learn JavaScript"
+let book = "A smarter way to learn JavaScript";
+
+// Display a message in an alert box
+alert("I am trying to learn the book: " + book);
+
+
+ //5. Generate the following message through browser’s developer cons
+        
+        document.write(name, "yah! i can write HTML content throught java script ");
+        // string in Variables
+        var myString = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+
+        // string in alert
+        alert (myString);
+
+        //string in the web page
+
+        document.write("<h1> " + myString +"</h1");
